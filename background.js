c39af000ed06eb2e2a4c24631aac7981244eef3b
Fabio Calefato <fabio.calefato@uniba.it>
2024-01-25 17:08:37 +0100

    Remove content script

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// create a context menu item to search dblp
 chrome.runtime.onInstalled.addListener(function () {
     let context = "selection"
     let title = "Search highlighted text on dblp";
@@ -9,6 +10,7 @@ chrome.runtime.onInstalled.addListener(function () {
     console.log('dblp search extension installed.');
 });
 
+// add menu click listener
 chrome.contextMenus.onClicked.addListener(function (info) {
     console.log('item ' + info.menuItemId + ' was clicked');
     if (info.menuItemId === "selection") {
