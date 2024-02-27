86c23c33c6d33b962d4388c99216ec62c9ba7f92
Fabio Calefato <fabio.calefato@uniba.it>
2024-02-27 16:48:07 +0100

    Fix image path in updateStatus function

--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,7 @@ function updateStatus(message, timeout) {
     var status = document.getElementById('status');
     if (status) {
         // append the spinner gif to status, size 16x16
-        status.innerHTML = message + '&nbsp;<img class="status" src="images/spinner.gif" />&nbsp;';
+        status.innerHTML = message + '&nbsp;<img class="status" src="../images/spinner.gif" />&nbsp;';
         setTimeout(function() {
             status.textContent = '';
         }, timeout);
