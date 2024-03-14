2c642f214188c5f100380c6303e1987457e150fa
Fabio Calefato <fcalefato@gmail.com>
2024-03-14 15:11:19 +0100

    Refactor to implement mvc pattern (closes #5)

---
+++ js/controller/controller.js
@@ -0,0 +1,25 @@
+// controller.js
+console.log("controller.js loaded");
+
+export class PublicationController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        this.model.subscribe(() => {
+            console.log('Controller received notification from model to update the view: ', 
+                        this.model.status, 
+                        this.model.totalHits, 
+                        this.model.sentHits);
+            this.view.update(this.model.status, 
+                            this.model.publications, 
+                            this.model.totalHits, 
+                            this.model.sentHits)
+        });
+    }
+
+    async handleSearch(query) {
+        console.log(`Controller received query: ${query}`);
+        await this.model.searchPublications(query);
+    }
+}
