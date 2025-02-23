1b3c529b0bfdd0b0387499056829a41813ceeabb
Fabio Calefato <fabio.calefato@uniba.it>
2025-02-23 09:51:32 +0100

    Fix XSS vulnerability in view.js
    Non-HTML variable 'table' is used to store raw HTML
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/bateman/dblp-search-ext?shareId=XXXX-XXXX-XXXX-XXXX).

--- js/view/view.js
+++ js/view/view.js
@@ -1,4 +1,4 @@
-// view.js
+ // view.js
 console.log("view.js loaded");
 var browser = browser || chrome;
 
@@ -30,24 +30,88 @@ export class PublicationView {
 
     // Build the table with the new data
     buildTable(publications) {
-        var table = '<table id="results-table" class="table table-striped table-hover">';
-        table += '<thead><tr><th scope="col" colspan="2">Title</th><th scope="col">Authors</th><th scope="col">Year</th><th scope="col">Venue</th><th scope="col">DOI</th><th scope="col">Access</th><th scope="col">BibTeX</th></tr></thead>';
-        table += '<tbody>';
-        publications.forEach((result) => {
-            table += '<tr>';
-            table += '<td><img class="' + result.type + '" title="' + result.type + '" src="../images/pub-type.png"></td>';
-            table += '<td><a href="' + result.permaLink + '" target="_blank" title="' + result.permalink + '">' + result.title + '</a></td>';
-            table += '<td>' + result.authors.join(', ') + '</td>';
-            table += '<td>' + result.year + '</td>';
-            table += '<td>' + result.venue + '</td>';
-            table += '<td><a href="' + result.doiURL + '" target="_blank">' + result.doi + '</a></td>';
-            table += '<td class="center"><img class="access" src="../images/' + result.access + '-access.png" title="This publication is ' + result.access + ' access"></td>';
-            table += '<td class="center"><button class="copyBibtexButton" title="Copy BibTex" data-url="' + result.bibtexLink + '"><img src="../images/copy.png"></button></td>';
-            table += '</tr>';
-        });
-        table += '</tbody>';
-        table += '</table>';
+        const table = document.createElement('table');
+        table.id = 'results-table';
+        table.className = 'table table-striped table-hover';
 
-        return table;
+        const thead = document.createElement('thead');
+        const headerRow = document.createElement('tr');
+        const headers = ['Title', 'Authors', 'Year', 'Venue', 'DOI', 'Access', 'BibTeX'];
+        headers.forEach(headerText => {
+            const th = document.createElement('th');
+            th.scope = 'col';
+            th.textContent = headerText;
+            headerRow.appendChild(th);
+        });
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        const tbody = document.createElement('tbody');
+        publications.forEach(result => {
+            const row = document.createElement('tr');
+
+            const typeCell = document.createElement('td');
+            const typeImg = document.createElement('img');
+            typeImg.className = result.type;
+            typeImg.title = result.type;
+            typeImg.src = '../images/pub-type.png';
+            typeCell.appendChild(typeImg);
+            row.appendChild(typeCell);
+
+            const titleCell = document.createElement('td');
+            const titleLink = document.createElement('a');
+            titleLink.href = result.permaLink;
+            titleLink.target = '_blank';
+            titleLink.title = result.permaLink;
+            titleLink.textContent = result.title;
+            titleCell.appendChild(titleLink);
+            row.appendChild(titleCell);
+
+            const authorsCell = document.createElement('td');
+            authorsCell.textContent = result.authors.join(', ');
+            row.appendChild(authorsCell);
+
+            const yearCell = document.createElement('td');
+            yearCell.textContent = result.year;
+            row.appendChild(yearCell);
+
+            const venueCell = document.createElement('td');
+            venueCell.textContent = result.venue;
+            row.appendChild(venueCell);
+
+            const doiCell = document.createElement('td');
+            const doiLink = document.createElement('a');
+            doiLink.href = result.doiURL;
+            doiLink.target = '_blank';
+            doiLink.textContent = result.doi;
+            doiCell.appendChild(doiLink);
+            row.appendChild(doiCell);
+
+            const accessCell = document.createElement('td');
+            accessCell.className = 'center';
+            const accessImg = document.createElement('img');
+            accessImg.className = 'access';
+            accessImg.src = `../images/${result.access}-access.png`;
+            accessImg.title = `This publication is ${result.access} access`;
+            accessCell.appendChild(accessImg);
+            row.appendChild(accessCell);
+
+            const bibtexCell = document.createElement('td');
+            bibtexCell.className = 'center';
+            const bibtexButton = document.createElement('button');
+            bibtexButton.className = 'copyBibtexButton';
+            bibtexButton.title = 'Copy BibTex';
+            bibtexButton.dataset.url = result.bibtexLink;
+            const bibtexImg = document.createElement('img');
+            bibtexImg.src = '../images/copy.png';
+            bibtexButton.appendChild(bibtexImg);
+            bibtexCell.appendChild(bibtexButton);
+            row.appendChild(bibtexCell);
+
+            tbody.appendChild(row);
+        });
+        table.appendChild(tbody);
+
+        return table.outerHTML;
     }
 }
